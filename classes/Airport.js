4040d2925fe5d5926cba78c186723b5691f43237
pmolina90 <P.a.molina90@gmail.com>
2024-02-08 16:36:59 -0500

    added code and test for AIRPORT

--- classes/Airport.js
+++ classes/Airport.js
@@ -1,6 +1,21 @@
 class Airport {
+    //airportCode: A static property with the three letter code for the airport (e.g. JFK or LHR).
+    static airportCode = "JFK";
+    #planes;
+    constructor(name){
+        this.name = name;
 
+        //planes: An array of Plane objects currently at the airport. Initializes as an empty array.
+        this.#planes =[];
+    }
 
+    getPlanes(){
+        return this.#planes;
+    }
+
+    addPlane(plane){
+        this.#planes.push(plane);
+    }
 }
 
 
