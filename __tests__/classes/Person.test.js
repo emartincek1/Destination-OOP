7c205f2a792182c1a5fbf88365510c03f340415b
Alex Evans <aevans@quiktrip.com>
2024-02-08 12:46:44 -0600

    created person class and tests

--- __tests__/classes/Person.test.js
+++ __tests__/classes/Person.test.js
@@ -5,8 +5,16 @@ describe("The class Person", () => {
     test("initalizes the name and destination properties correctly", () => {
         let t1 = new Person("John", "New York");
         expect(t1.name).toBe("John");
-        expect(location).toBe("New York");
+        expect(t1.destination).toBe("New York");
     })
 
+    test("initializes bag as an empty variable", () => {
+        let t1 = new Person("John", "New York");
+        expect(t1.getBags()).toEqual([])
+    })
 
+    test("addBag method adds a bag to array", () => {
+        let t1 = new Person("John", "New York");
+        expect(t1.addBag("Gucci")).toEqual(["Gucci"])
+    })
 })
--- classes/Person.js
+++ classes/Person.js
@@ -1,7 +1,22 @@
 class Person {
+    #bag 
+    constructor(name, destination){
+        this.name = name;
+        this.destination = destination;
+        this.#bag = [];
+    }
 
+    addBag(bag){
+        this.#bag.push(bag);
+        return this.#bag
+    }
+
+    getBags(){
+        return this.#bag
+    }
 
 }
+
 
 
 module.exports = Person;
